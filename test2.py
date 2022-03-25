6cefcbe9e9fd194a3d2c8acca3aa0317ab6336a5
Darkpuzzy <zhirkov_lol@mail.ru>
2022-03-25 17:03:50 +0300

    Add steam store parse

--- test2.py
+++ test2.py
@@ -4,6 +4,8 @@ import asyncio
 import aiohttp
 import websockets
 import requests
+from lxml import etree
+from urllib.request import urlopen
 
 FUA = UserAgent().chrome
 
@@ -52,6 +54,88 @@ Elden Ring, Fallout New Vegas, Rising Storm 2 Vietnam
 #     async with websockets.connect(url_1) as client:
 #         print(await client.recv())
 
+async def links(name, tree):
+
+    list_links = []
+
+    for n in range(1, 20):
+        j = tree.xpath(f"//div[@id='search_resultsRows']/a[{n}]")
+        advacii = j[-1].attrib
+        if name in advacii['href']:
+            list_links.append(advacii['href'])
+        elif name.capitalize() in advacii['href']:
+            list_links.append(advacii['href'])
+        elif name.upper() in advacii['href']:
+            list_links.append(advacii['href'])
+
+    if not list_links:
+        interim_list = []
+        j1 = tree.xpath(f"//div[@id='search_resultsRows']/a[1]")
+        advac = j1[-1].attrib
+        interim_list.append(advac['href'])
+        tasks = asyncio.create_task(spliter(list_cheak=interim_list, game_name=None))
+        game_name = await asyncio.gather(tasks)
+        enter = game_name[0]
+        return await links(name=enter, tree=tree)
+    # print(list_links)
+    return list_links
+
+
+async def spliter(list_cheak, game_name):
+    list_split = []
+    n = 0
+    game_n = await valid(game_name)
+    while n < len(list_cheak):
+        for i in list_cheak:
+            j = i.split('/')
+            list_split.extend(j)
+            n += 1
+
+    if game_n != None:
+
+        async def find_game_id(list_cheaker, game):
+            try:
+                index_game = list_cheaker.index(game)
+                game_id = list_cheaker[index_game - 1]
+                return game_id
+            except ValueError:
+                ind = 0
+                while True:
+                    if game.lower() == list_cheaker[ind].lower():
+                        game_id = list_cheaker[ind-1]
+                        break
+                    else:
+                        ind += 1
+                        continue
+            return game_id
+        return await find_game_id(list_cheaker=list_split, game=game_n)
+    else:
+        return list_split[5]
+
+
+async def linkers(id, list_game):
+    for j in list_game:
+        if id in j:
+            return j
+
+
+async def valid(name):
+    if isinstance(name, str):
+        text_for_site = name
+        list_split = text_for_site.split(" ")
+        list_to_join = "_".join(list_split)
+        list_to_str = list_to_join
+        return list_to_str
+
+
+async def validlink(name):
+    if isinstance(name, str):
+        text_for_site = name
+        list_split = text_for_site.split(" ")
+        list_to_join = "+".join(list_split)
+        list_to_str = list_to_join
+        return list_to_str
+
 
 async def find(obj, to_append):
     for i in obj:
@@ -79,7 +163,25 @@ class Game:
     def __init__(self, text):
         self.text = text
 
-    async def is_valid(self):
+    ''' parsing steam store '''
+
+    async def steam_find(self):
+        steam = 'https://store.steampowered.com/search/?term={}'.format(await validlink(name=self.text))
+        HTMLPARCE = etree.HTMLParser()
+        req_link = requests.get(steam, headers={'User-Agent': FUA})
+        codetxt = req_link.text
+        with open('SteamHTML', 'w', encoding='utf-8') as f:
+            f.write(codetxt)
+        local = 'file:///C:/Users/user/AioAsync/SteamHTML'
+        respones = urlopen(local)
+        treex = etree.parse(respones, HTMLPARCE)
+        task = asyncio.create_task(linkers(id=await spliter(await links(name=self.text, tree=treex), game_name=self.text), list_game=await links(name=self.text, tree=treex)))
+        res1 = await asyncio.gather(task)
+        return res1[0]
+
+    ''' parsing zaka-zaka store '''
+
+    async def zaka_find(self):
         if isinstance(await validator(text=self.text), str):
             try:
                 zaka = 'https://zaka-zaka.com/game/{}'.format(await validator(text=self.text))
@@ -89,8 +191,8 @@ class Game:
                 price_now = soup.find_all('div', class_='price')
                 discount = soup.find_all('div', class_='discount')
                 tasks = asyncio.create_task(price_game(oldprice=old_price, price=price_now, discount=discount, site_url=zaka))
-                res = await asyncio.gather(tasks)
-                return res[0] # В данном случае получаем фильтрованные данные
+                res2 = await asyncio.gather(tasks)
+                return res2[0] # В данном случае получаем фильтрованные данные
             except IndexError:
                 return """ 
                     Enter the name of the game like this:\n
@@ -98,6 +200,10 @@ class Game:
                             """
         else:
             return print('Sorry')
+
+    async def finally_stand(self, res1, res2):
+        pass
+
 
 
 async def test(text):
@@ -109,11 +215,12 @@ async def test(text):
 
 async def main(text):
     gm = Game(text=text)
-    return await gm.is_valid()
-
+    res1 = await gm.zaka_find()
+    res2 = await gm.steam_find()
+    return f'Zaka-Zaka Store:\n{res1}\nSteam Store:\n{res2}'
 
 if __name__ == '__main__':
-    txt_input = input()
+    txt_input = 'Elden Ring'
     loop = asyncio.get_event_loop()
     tasks = main(txt_input)
     print(loop.run_until_complete(tasks)) # Elden Ring, Rising Storm 2 Vietnam, Fallout 3, Fallout New Vegas
