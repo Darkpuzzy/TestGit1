7afef44966c4bf3a885ee3bd671e364b7061623b
Darkpuzzy <zhirkov_lol@mail.ru>
2022-03-29 19:19:32 +0300

    testing websockets

--- test3.py
+++ test3.py
@@ -1,12 +1,25 @@
 import aiohttp
 import asyncio
+import websockets
 
-async def main():
+
+async def main1():
 
     async with aiohttp.ClientSession() as session:
         async with session.get('https://zaka-zaka.com/game/rising-storm-2-vietnam') as response:
             html = await response.text()
-            print(html)
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
+loop.run_until_complete(main1())
+
+
+async def main2():
+
+    uri = f"wss://stream.binance.com:9443/stream?streams=btcusd@miniTicker"
+    async with websockets.connect(uri) as client:
+        while True:
+            res = await client.recv()
+            print(res)
+
+loop1 = asyncio.get_event_loop()
+loop1.run_until_complete(main2())
